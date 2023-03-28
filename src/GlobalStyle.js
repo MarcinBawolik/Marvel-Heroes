e8e35888dfd15723e3d27d9bea1e0474c7d3f75f
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-28 11:31:27 +0200

    added GlobalStyle to application

---
+++ src/GlobalStyle.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+*{
+    padding: 0;
+    margin: 0;
+}
+
+body {
+    background-color: ${({ theme }) => theme.color.black};
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`;
