5b33aa53d0840049b0b39d70f0f2ecf89ad4ac0d
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-04-03 14:18:23 +0200

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Main } from "./Components/Main";
+import { Route, Routes } from "react-router-dom";
+import { Marvel } from "./MarvelHeroes";
 
 function App() {
   return (
     <>
-      <Main />
+      <Routes>
+        <Route path="/Marvel-Heroes" element={<Main />} />
+        <Route path="/:id" element={<Marvel />} />
+      </Routes>
     </>
   );
 }
